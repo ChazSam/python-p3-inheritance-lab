52e5c6b268ca47791e127bb60ffc2b013e1e8f64
Charles <Charlessammarco@gmail.com>
2023-11-29 13:28:48 -0600

    inheritance 1

--- lib/teacher.py
+++ lib/teacher.py
@@ -3,8 +3,19 @@
 from user import User
 
 import random
+import ipdb
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name, knowledge=["Art","Music"]):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+        
+        
+
     def teach(self):
-        pass
+        # self.teach = teach
+        # ipdb.set_trace()
+        print(self.knowledge)
+        return self.knowledge[random.randint(0, len(self.knowledge)-1)]
+    
